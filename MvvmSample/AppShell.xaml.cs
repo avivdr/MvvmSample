6efbbdb22ef5f7b1969d67bce87ea3a6013eb69e
User <User@LAB3-3>
2023-05-18 14:33:33 +0300

    qw

--- MvvmSample/AppShell.xaml.cs
+++ MvvmSample/AppShell.xaml.cs
@@ -7,6 +7,8 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-		Routing.RegisterRoute("DetailsPage", typeof(StudentDetails));
-	}
+		//Routing.RegisterRoute("DetailsPage", typeof(StudentDetails));
+  //      Routing.RegisterRoute("DetailsPage", typeof(TeacherPage));
+  //      Routing.RegisterRoute("DetailsPage", typeof(MainPage));
+    }
 }
